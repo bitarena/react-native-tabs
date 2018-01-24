1d52f48cbe4f1aa70dbe6cabbb76bf2a95d8ffd6
David Muro <david.muro9@gmail.com>
2018-01-24 19:30:55 +0100

    tabs working! no header, no navbar nothing!

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-// app/index.js
-
 import React, { Component } from 'react';
 import {
   StyleSheet,
@@ -9,21 +7,65 @@ import {
 import { Router, Scene } from 'react-native-router-flux';
 import ScarletScreen from './src/ScarletScreen';
 import GrayScreen from './src/GrayScreen';
+import GoldScreen from './src/GoldScreen';
+
+const TabIcon = ({ selected, title }) => {
+  return (
+    <Text style={{color: selected ? 'red' :'black'}}>{title}</Text>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <Scene key="root">
-        <Scene key="scarlet"
-          component={ScarletScreen}
-          title="Scarlet"
-          initial
-        />
         <Scene
-          key="gray"
-          component={GrayScreen}
-          title="Gray"
-        />
+          key="tabbar"
+          tabs={true}
+          tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+          tabBarPosition={'bottom'}
+          hideNavBar
+        >
+          <Scene
+            key="osu" 
+            title="Tab1" 
+            icon={TabIcon}
+            hideNavBar
+          >
+            <Scene 
+              key="scarlet"
+              component={ScarletScreen}
+              title="Scarlet"
+              initial
+            />
+          </Scene>
+
+          <Scene
+            key="um"
+            title="Tab2"
+            icon={TabIcon}
+            hideNavBar
+          >
+            <Scene
+              key="gray"
+              component={GrayScreen}
+              title="Gray"
+            />
+          </Scene>
+
+          <Scene 
+            key="vu" 
+            title="Tab3"
+            icon={TabIcon}
+            hideNavBar
+          >
+            <Scene
+              key="gold"
+              component={GoldScreen}
+              title="Gold"
+            />
+          </Scene>
+        </Scene>
       </Scene>
     </Router>
   );
